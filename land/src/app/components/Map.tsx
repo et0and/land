c75f32b9495be1217166d8a410cd3c712ed8a262
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-01 18:33:46 +1200

    Styling

--- land/src/app/components/Map.tsx
+++ land/src/app/components/Map.tsx
@@ -1,18 +1,19 @@
-'use client'
-
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import MapIcon from './MapIcon';
-import Image from 'next/image';
+"use client";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import MapIcon from "./MapIcon";
+import Image from "next/image";
 
 type Location = {
   id: string;
   name: string;
   lat: number;
   lng: number;
-  audioSrc: string;
+  audioSrc?: string;
+  videoSrc?: string;
   imageSrc?: string; // Optional image source
   imageAlt?: string; // Optional image alt text
+
   description?: string; // Optional description, if needed
 };
 
@@ -25,34 +26,42 @@ const Map: React.FC<MapProps> = ({ locations }) => {
     <>
       <MapIcon />
       <MapContainer
-        center={[0, 0]}
-        zoom={2}
-        style={{ height: '100vh', width: '100%' }}
+        center={[-41.184442, 174.94956]}
+        zoom={7}
+        style={{ height: "100vh", width: "100%" }}
+        minZoom={4}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | <a href="https://tom.so">Tom Hackshaw</a>'
+        <TileLayer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
         />
         {locations.map((location) => (
           <Marker key={location.id} position={[location.lat, location.lng]}>
-            <Popup 
-              className="custom-popup"
-              closeButton={false}
-            >
+            <Popup className="custom-popup" closeButton={false}>
               <div>
-                <h1 className="font-medium text-lg pb-4">{location.name}</h1>
+                <h1 className="font-medium text-lg">{location.name}</h1>
                 {location.imageSrc && (
                   <div className="mb-2">
                     <Image
                       src={location.imageSrc}
                       alt={location.imageAlt || location.name}
-                      width={200}
+                      width={300}
                       height={150}
                       objectFit="cover"
                     />
                   </div>
                 )}
                 {location.description && (
-                <p className="text-sm">{location.description}</p>)}
-                <audio controls src={location.audioSrc} />
+                  <p className="text-sm">{location.description}</p>
+                )}
+                {location.audioSrc && (
+                  <audio controls src={location.audioSrc} />
+                )}
+                {location.videoSrc && (
+                  <video controls width={300} className="p-2">
+                    <source src={location.videoSrc} type="video/mp4" />
+                  </video>
+                )}
               </div>
             </Popup>
           </Marker>
--- land/src/app/page.tsx
+++ land/src/app/page.tsx
@@ -1,36 +1,36 @@
-import dynamic from 'next/dynamic';
-import { ClipLoader } from 'react-spinners';
+import dynamic from "next/dynamic";
+import { ClipLoader } from "react-spinners";
 
-const Map = dynamic(() => import('./components/Map'), {
+const Map = dynamic(() => import("./components/Map"), {
   ssr: false,
-  loading: () => <div className="flex justify-center items-center h-screen">
-  <ClipLoader color="#4A90E2" size={50} />
-</div>
+  loading: () => (
+    <div className="flex justify-center items-center h-screen">
+      <ClipLoader color="#4A90E2" size={50} />
+    </div>
+  ),
 });
 
 const locations = [
   {
-    id: '1',
-    name: 'Location 1',
-    lat: 51.505,
-    lng: -0.09,
-    audioSrc: '/audio/sample1.mp3',
-    imageSrc: '/images/location1.jpg',
-    imageAlt: 'Image of Location 1',
-    description: 'This is a description.'
+    id: "1",
+    name: "-41.2802, 174.775696",
+    lat: -41.2802,
+    lng: 174.775696,
+    videoSrc: "/land/video/test.mp4",
+    description: "Filmed Tuesday 2 July, 2024.",
   },
   {
-    id: '2',
-    name: 'Location 2',
+    id: "2",
+    name: "Location 2",
     lat: 40.7128,
-    lng: -74.0060,
-    audioSrc: '/audio/sample2.mp3',
+    lng: -74.006,
+    audioSrc: "/audio/sample2.mp3",
   },
 ];
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-black">
       <Map locations={locations} />
     </main>
   );
