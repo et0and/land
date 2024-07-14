74bc0d3c4fcdeb0bd1636576a9d2cff7111a4ee7
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-14 18:10:04 +1200

    ids

--- land/src/app/page.tsx
+++ land/src/app/page.tsx
@@ -12,24 +12,28 @@ const Map = dynamic(() => import("./components/Map"), {
 
 const locations = [  
   {
+    id: 1,
     lat: -41.2829895,
     lng: 174.7765351,
     imageSrc: "/land/images/3091.jpeg",
     imageAlt: "Birds sitting on top of office building",
   },
   {
+    id: 2,
     lat: -36.8660046,
     lng: 174.7422192,
     imageSrc: "/land/images/3231.jpeg",
     imageAlt: "A handsome, young, successful man points at a computer screen with the Discord app open.",
   },
   {
+    id: 3,
     lat: -36.625204, 
     lng: 174.740450,
     imageSrc: "/land/images/IMG_0288.jpeg",
     imageAlt: "A blurry image of a young man laughing in motion.",
   },
   {
+    id: 4,
     lat: -36.754524, 
     lng: 174.698166,
     imageSrc: "/land/images/IMG_2275.jpeg",
